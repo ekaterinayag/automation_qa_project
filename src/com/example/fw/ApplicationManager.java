af1748601b832c4a3b7fb8f99e7ce87a9b7c364b
ekaterinayag <kotobox@mail.ru>
2014-11-11 18:44:18 +0300

    Home Task#5 - ДЗ14
    properties files usage

--- src/com/example/fw/ApplicationManager.java
+++ src/com/example/fw/ApplicationManager.java
@@ -1,9 +1,13 @@
 package com.example.fw;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.io.File;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class ApplicationManager {
 	
@@ -12,12 +16,28 @@ public class ApplicationManager {
 	private NavigationHelper navigationHelper;
 	private GroupHelper groupHelper;
 	private ContactHelper contactHelper;
+	private Properties properties;
 	
-	public ApplicationManager() {
-		driver = new FirefoxDriver();
-	    baseUrl = "http://localhost/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    driver.get(baseUrl + "/addressbookv4.1.4/");	    
+	public ApplicationManager(Properties properties) {
+		this.properties = properties;
+		String browser = properties.getProperty("browser");
+		if("firefox".equals(browser)) {
+			driver = new FirefoxDriver();
+		} else if ("ie".equals(browser)) {
+			File file = new File("D:/Java/IEDriverServer.exe");
+			System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+			driver = new InternetExplorerDriver();
+			
+		} else if ("chrome".equals(browser)) {
+			File file = new File("D:/Java/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+			driver = new ChromeDriver();
+		} else {
+			throw new Error("Unsupported browser:" + browser);
+		}
+		baseUrl = properties.getProperty("baseUrl");
+	   // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.get(baseUrl);	    
 	}
 	
 	public void stop() {
--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -1,8 +1,14 @@
 package com.example.tests;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+
+import java.util.Properties;
+import java.util.Random;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,18 +16,17 @@ import org.testng.annotations.DataProvider;
 
 import com.example.fw.ApplicationManager;
 
-import static com.example.tests.GroupDataGenerator.generateRandomGroups;
-import static com.example.tests.ContactDataGenerator.generateRandomContacts;
-
 public class TestBase {
 	
-	protected static ApplicationManager app;
+	protected ApplicationManager app;
 
 
 	@BeforeTest
 	public void setUp() throws Exception {
-		app = new ApplicationManager();
-	    
+		String configFile = System.getProperty("configFile", "application.properties");
+		Properties properties = new Properties();
+		properties.load(new FileReader(new File(configFile)));
+		app = new ApplicationManager(properties);
 	  }
 	
 	@AfterTest
