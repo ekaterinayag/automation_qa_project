0206161b99f0c95059939e8fa884b252d9bd3c54
ekaterinayag <kotobox@mail.ru>
2014-11-14 22:59:17 +0300

    Home Task#5 - ДЗ13

---
+++ Contacts generate to xml.bat
@@ -0,0 +1,1 @@
+java -cp bin;D:\Java\xstream-distribution-1.4.7-bin\xstream-1.4.7\lib\xstream-1.4.7.jar;D:\Java\xstream-distribution-1.4.7-bin\xstream-1.4.7\lib\xstream\xpp3_min-1.1.4c.jar;D:\Java\xstream-distribution-1.4.7-bin\xstream-1.4.7\lib\xstream\xmlpull-1.1.3.1.jar;D:\Java\selenium-server-standalone-2.43.1.jar com.example.tests.ContactDataGenerator 3 contacts.xml xml
